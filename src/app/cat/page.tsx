68b53dfa7a2858e1cf7c61a0a62f5c24ce158822
chengchao <chengchao60827@gmail.com>
2024-01-11 19:52:54 +0800

    use react-images-uploading package

--- src/app/cat/page.tsx
+++ src/app/cat/page.tsx
@@ -1,7 +1,59 @@
+import React from "react";
+import ImageUploading, { ImageListType, ImageType } from 'react-images-uploading';
+
 export default function Page() {
+  const [images, setImages] = React.useState<ImageListType>([]);
+  const maxNumber = 30;
+
+  const onChange = (imageList: ImageListType, addUpdateIndex?: number[]) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
   return <>
     <div>
-      <input type="file" accept="image/*" multiple />
+      {/* <input type="file" accept="image/*" multiple onChange={handleChange} /> */}
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+        acceptType={["image/*"]}
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
     </div>
   </>
 }
